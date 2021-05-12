c745890b00e667866ffa3c008734476920d6f988
Lolomyn <61377364+Lolomyn@users.noreply.github.com>
2021-05-12 15:22:18 +0300

    xzg

--- cur/src/app/system/planning-page/planning-page.component.ts
+++ cur/src/app/system/planning-page/planning-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {AddService} from '../shared/services/add.service';
 import {StudyingService} from '../shared/services/studying.service';
-import {AddpatientModel} from '../shared/models/adduser.model';
+// import {AddpatientModel} from '../shared/models/adduser.model';
 
 @Component({
   selector: 'app-planning-page',
@@ -11,15 +11,15 @@ import {AddpatientModel} from '../shared/models/adduser.model';
 })
 export class PlanningPageComponent implements OnInit {
   form: FormGroup;
-  studying = this.studyingService.getStudying(); patients = this.studyingService.getPatients();
+  studying = this.studyingService.getStudying(); // patients = this.studyingService.getPatients();
   constructor(private addService: AddService, private studyingService: StudyingService) { }
 
   // changePatient(pat: AddpatientModel): void {
   //   this.patientService.changePatient(pat).subscribe();
   // }
-  deletePatient(pat: AddpatientModel): void {
-    this.studyingService.deletePat(pat).subscribe();
-  }
+  // deletePatient(pat: AddpatientModel): void {
+  //   this.studyingService.deletePat(pat).subscribe();
+  // }
   ngOnInit() {
     this.form = new FormGroup({
       Patients: new FormControl(),
